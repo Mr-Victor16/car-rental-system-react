03ba13b179f7b50665dcce930eab49bddd1997d6
MrVictor16 <cezary.wiktorowicz.98@gmail.com>
2023-03-25 20:34:59 +0100

    Usunięcie roli moderatora

--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -1,11 +1,10 @@
-import { React, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Box, Button } from '@mui/material';
 import AuthService from '../services/AuthService';
 
 const Navigation = () => {
     const [ user, setUser ] = useState('');
-    const [ moderator, setModerator ] = useState(false);
     const [ admin, setAdmin ] = useState(false);
     let navigate = useNavigate();
 
@@ -13,7 +12,6 @@ const Navigation = () => {
         const currentUser = JSON.parse(localStorage.getItem("user"));
         
         if (currentUser) {
-            setModerator(currentUser.roles.includes("ROLE_MODERATOR"));
             setAdmin(currentUser.roles.includes("ROLE_ADMIN"));
             setUser(currentUser);
         }
@@ -21,8 +19,7 @@ const Navigation = () => {
 
     const logOut = () => {
         AuthService.logout();
-        
-        setModerator(false);
+
         setAdmin(false);
         setUser(undefined);
         
